0ef31c099260afa2f8139394efd6c734d96c544e
Default-Mat <matin.arno4646@gmail.com>
2023-02-20 16:02:09 +0330

    commit on smtp br

--- scrape.py
+++ scrape.py
@@ -4,63 +4,81 @@ from selenium import webdriver
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as ec
-from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+import smtplib
+import ssl
 
 
 def showTickets(tickets):
     if len(tickets) == 0:
-        print("####################################")
-        print("\nThere are " + str(len(trains)) + " tickets available\n")
-        print("####################################")
+        return "no tickets"
 
     else:
-        print("####################################")
-        print("There are " + str(len(trains)) + " tickets available\n")
-        for i in tickets:
-            train_type = i.find("span", class_='train-type')
-            train_departure = i.find("div", class_='departure')
-            train_entrance = i.find("div", class_='entrance')
-            departure_time = train_departure.find("span", class_='titleModal')
-            entrance_time = train_entrance.find("span", class_='titleModal')
-            price = i.find("div", class_='price titleModal')
-            print(train_type.text + ' ' + departure_time.text + ' ' + entrance_time.text)
-            print(price.text + '\n')
-        print("####################################")
+        string = f"""####################################
+There are {str(len(tickets))} tickets available\n"""
 
+        # for i in tickets:
+        #     train_type = i.find("span", class_='train-type')
+        #     train_departure = i.find("div", class_='departure')
+        #     train_entrance = i.find("div", class_='entrance')
+        #     departure_time = train_departure.find("span", class_='titleModal')
+        #     entrance_time = train_entrance.find("span", class_='titleModal')
+        #     price = i.find("div", class_='price titleModal')
+        #     string += f"{train_type.text} {departure_time.text} {entrance_time.text}\n{price.text}\n"
+    return string
+
+
+def sendMail(Sender, Password, Receiver, Message):
+    context = ssl.create_default_context()
+    smtp = smtplib.SMTP('smtp-mail.outlook.com', 587)
+    smtp.ehlo()
+    smtp.starttls(context=context)
+    smtp.ehlo()
+    smtp.login(Sender, Password)
+    smtp.sendmail(Sender, Receiver, Message)
+    print('email sent!')
+
+
+choice = input("\n1.shahrud-mashhad\n2.mashhad-shahrud\n3.others\n4.exit\nChoose: ")
+
+if choice == '1':
+    beg = "shahrud"
+    dest = "mashhad"
+
+elif choice == '2':
+    beg = "mashhad"
+    dest = "shahrud"
+
+elif choice == '3':
+    beg = input("enter the beginning point: ")
+    dest = input("enter the destination: ")
+
+else:
+    os.system('exit')
+    exit()
+
+date = input("enter the date in yyyy-mm-dd format: ")
+
+url = "https://ghasedak24.com/search/train/" + beg + "-" + dest + "/" + date + "/1/0/0/1/"
+print("Please wait.......")
+
+sender = 'matin.arno4646@outlook.com'
+password = '34129093428198matin'
+receiver = 'matin.geralt6565@gmail.com'
+message = f"""From: From Person <matin.arno4646@outlook.com>
+To: To Person <matin.geralt6565@gmail.com>
+Subject: SMTP e-mail test
+
+This is a test e-mail message.\n"""
 
 while True:
-    choice = input("\n1.shahrud-mashhad\n2.mashhad-shahrud\n3.others\n4.exit\nChoose: ")
-    os.system('cls')
-
-    if choice == '1':
-        beg = "shahrud"
-        dest = "mashhad"
-
-    elif choice == '2':
-        beg = "mashhad"
-        dest = "shahrud"
-
-    elif choice == '3':
-        beg = input("enter the beginning point: ")
-        dest = input("enter the destination: ")
-
-    else:
-        os.system('exit')
-        break
-
-    date = input("enter the date in yyyy-mm-dd format: ")
-
-    url = "https://ghasedak24.com/search/train/" + beg + "-" + dest + "/" + date + "/1/0/0/1/"
-    print("Please wait.......")
-
     capa = DesiredCapabilities.CHROME
     capa["pageLoadStrategy"] = "none"
     options = webdriver.ChromeOptions()
-    options.headless = True
-    driver = webdriver.Chrome(desired_capabilities=capa,
-                              options=options)
-    wait = WebDriverWait(driver, 20)
+    options.add_argument('--headless=new')
+    driver = webdriver.Chrome(desired_capabilities=capa, options=options)
+    wait = WebDriverWait(driver, 30)
     driver.get(url)
 
     try:
@@ -68,12 +86,18 @@ while True:
         driver.execute_script("window.stop();")
         soup = BeautifulSoup(driver.page_source, "html.parser")
         trains = soup.find_all("div", class_="panel panel-default tkpnl element-item trainPanel")
-        showTickets(trains)
+        foundTickets = showTickets(trains)
         driver.close()
+        if foundTickets == "no tickets":
+            print("There are 0 tickets available")
+
+        else:
+            message += f"{foundTickets}"
+            sendMail(sender, password, receiver, message)
+
+        break
+
     except:
         print("\nNo tickets available at the moment")
         driver.close()
-
-    input("\npress enter to continue...")
-    os.system('cls')
-    continue
+        break
